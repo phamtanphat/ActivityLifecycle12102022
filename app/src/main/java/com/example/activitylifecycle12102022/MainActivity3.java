220172bb6d5cc0d3eb24ada3e36d2f0526d04377
Phat <phatdroid94@gmail.com>
2022-11-28 18:10:14 +0700

    single task

--- app/src/main/java/com/example/activitylifecycle12102022/MainActivity3.java
+++ app/src/main/java/com/example/activitylifecycle12102022/MainActivity3.java
@@ -2,16 +2,31 @@ package com.example.activitylifecycle12102022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    Button btnNavigateScreen3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         Log.d("BBB", "Main3: onCreate");
+
+        btnNavigateScreen3 = findViewById(R.id.button_screen3);
+
+        btnNavigateScreen3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
